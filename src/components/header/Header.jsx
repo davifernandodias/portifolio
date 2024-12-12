c150715aad80788293a62927809483f61e42044f
Davi Dias <davifernandosousadias18@gmail.com>
2024-12-12 20:14:24 -0300

    styles: adicionando hover nos icones do component header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,39 @@
 import './header.css';
 import { IoMdHome } from "react-icons/io";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { MdContactPage } from "react-icons/md";
 import ButtonContact from '../ux/button-contact/Button-contact';
-
-
-
-
 
 function Header() {
     return (
         <header className='header-content'>
             <div className='header-item'>
-            <IoMdHome className='home-icon'/>
-            <FaLinkedin  className='home-icon'/>
-            <FaGithub className='home-icon'/>
-            <MdContactPage className='home-icon'/>
-            <div class="space-line"></div>
-            <ButtonContact/>
+                <IoMdHome className='home-icon'/>
+                <div className="tooltip">Home</div>
             </div>
+
+            <div className='space-line'></div>
+
+            <div className='header-item'>
+                <FaLinkedin className='home-icon'/>
+                <div className="tooltip">LinkedIn</div>
+            </div>
+
+            <div className='header-item'>
+                <FaGithub className='home-icon'/>
+                <div className="tooltip">GitHub</div>
+            </div>
+
+            <div className='header-item'>
+                <MdContactPage className='home-icon'/>
+                <div className="tooltip">Contact</div>
+            </div>
+
+            <div className='space-line'></div>
+
+            <ButtonContact />
         </header>
-    )
+    );
 }
 
 export default Header;
