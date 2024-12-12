4c49c5eefa865d11d991d1fd09c1383e523e31e2
Davi Dias <davifernandosousadias18@gmail.com>
2024-12-12 16:24:05 -0300

    feat: criação de component container

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,13 @@
+import Header from "../header-content/Header";
+import TextProfile from '../ux/titleText/TextProfile';
+
+function Container() {
+    return (
+        <div className="container-content">
+            <Header />
+            <TextProfile />
+        </div>
+    );
+}
+
+export default Container;
