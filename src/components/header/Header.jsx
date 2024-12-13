61c6d0743e447ee394bb414a0f6bb76d69bae61b
Davi Dias <davifernandosousadias18@gmail.com>
2024-12-13 09:57:52 -0300

    refactor: adicionado evento de redirecionamento para os links dos seus repectivos item-icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,8 @@ import { MdContactPage } from "react-icons/md";
 import ButtonContact from '../ux/button-contact/Button-contact';
 
 function Header() {
+
+
     return (
         <header className='header-content'>
             <div className='header-item'>
@@ -15,17 +17,21 @@ function Header() {
             <div className='space-line'></div>
 
             <div className='header-item'>
-                <FaLinkedin className='home-icon'/>
+            <FaLinkedin 
+                className='home-icon' 
+                onClick={() => window.open('https://www.linkedin.com/in/davifernandodias/', '_blank')}/>
                 <div className="tooltip">LinkedIn</div>
             </div>
 
             <div className='header-item'>
-                <FaGithub className='home-icon'/>
+                <FaGithub className='home-icon'
+                onClick={() => window.open('https://github.com/davifernandodias', '_blank')}/>
                 <div className="tooltip">GitHub</div>
             </div>
 
             <div className='header-item'>
-                <MdContactPage className='home-icon'/>
+                <MdContactPage className='home-icon'
+                onClick={() => window.location.href = 'https://download937.mediafire.com/2iabgnehno3gaLgGgdSe5ZAr2cgxgM5fTiFYdfifJpU6qcHh7g9CJUAZf19sZxhk2MHw0prNIzUJQC9dY_-NqnmUkDzeWqknVTFzmnu-SBbCTuOrMyzBpr8_boJc_VkCiwcdkYNBNhtj937uTIF6JdauAowX4OM2q6HWF8u56JCO9Do/4y6x4bk6dqzwd40/curriculodavidias2025.pdf'}/>
                 <div className="tooltip">Contact</div>
             </div>
 
