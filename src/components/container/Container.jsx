3d6c6f80862e1c8e73a5f1a8877f218a99bd6b1a
Davi Dias <davifernandosousadias18@gmail.com>
2024-12-15 13:08:31 -0300

    feat: atribuindo responsividade para o container se encaixar em diversas resoluções

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,13 +1,17 @@
 import './container.css';
 import Header from "../header/Header";
 import ProfileDescription from '../profile-description/Profile-description';
+import HeaderMobile from '../ux/header-mobile/Header-Mobile';
 
 function Container() {
     return (
+        <>
+            <HeaderMobile/> 
         <section className="container-content">
             <Header />
             <ProfileDescription />
         </section>
+        </>
     );
 }
 
