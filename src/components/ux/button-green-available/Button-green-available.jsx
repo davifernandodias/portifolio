7b81fab60825c2b85cc24dc69162a7b5fd7d7432
Davi Dias <davifernandosousadias18@gmail.com>
2024-12-16 07:34:13 -0300

    feat: criação do button de disponível

---
+++ src/components/ux/button-green-available/Button-green-available.jsx
@@ -0,0 +1,14 @@
+import './button-green.css';
+
+function ButtonGreenAvailable() {
+    return (
+        <>
+            <button className='btn-available'>
+                <div className='point-alter-green-light'></div>
+                Disponível
+            </button>
+        </>
+    );
+}
+
+export default ButtonGreenAvailable;
